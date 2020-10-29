9992e30f7acc8562d3fe2ec1a580c9ef876d7ffd
fluor <pawel.d.dabrowski@gmail.com>
2020-10-29 19:44:44 +0100

    added i18n and some messages properties

--- src/main/java/com/facebook/games/Main.java
+++ src/main/java/com/facebook/games/Main.java
@@ -5,10 +5,28 @@ import com.facebook.games.lotek.LotekGame;
 import com.facebook.games.utils.ScannerWrapper;
 import com.facebook.games.utils.ScannerWrapperImpl;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String language;
+        String country;
+
+        if (args.length != 2) {
+            language = new String("en");
+            country = new String("US");
+        } else {
+            language = new String(args[0]);
+            country = new String(args[1]);
+        }
+
+        Locale currentLocale;
+        ResourceBundle messages;
+
+        currentLocale = new Locale(language, country);
+
         try (Scanner scanner = new Scanner(System.in)){
             ScannerWrapper scannerWrapper = new ScannerWrapperImpl(scanner);
             GameWithScanner game = new LotekGame();
